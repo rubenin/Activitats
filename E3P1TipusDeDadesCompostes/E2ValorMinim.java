8caa3936a2e45ee625906e3b58be5f32d04b9099
Ruben <rubengonzalezglez@me.com>
2023-01-28 10:34:19 +0100

    Terminado E2 Valor Minim

--- E3P1TipusDeDadesCompostes/E2ValorMinim.java
+++ E3P1TipusDeDadesCompostes/E2ValorMinim.java
@@ -1,6 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+/** L'objectiu d'aquesta activitat és aprendre a recórrer un array per tractar els
+ * valors que conté.
+ *
+ * Genereu un programa en Java anomenat ValorMinim que calculi quin és el valor
+ * mínim d'un array de nombres reals i mostri per pantalla. L'array per tractar
+ * pot ser qualsevol i el podeu declarar com una constant dins del codi font.
  */
 package E3P1TipusDeDadesCompostes;
 
@@ -9,5 +12,17 @@ package E3P1TipusDeDadesCompostes;
  * @author ruben
  */
 public class E2ValorMinim {
-    
+
+    public static final double[] array = {1, -2.2, 6.7, 4, 23.5, -5.5, 4.43, 20};
+
+    public static final void main(String[] args) {
+
+        double minim = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < minim) {
+                minim = array[i];
+            }
+        }
+        System.out.println("El valor mínim és " + minim + ". ");
+    }
 }
