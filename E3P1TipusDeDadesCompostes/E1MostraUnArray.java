57956b9568e4e24bff6bb9264df100d3e2b0d59b
Ruben <rubengonzalezglez@me.com>
2023-01-28 10:27:42 +0100

    Terminado E1 Mostra un array

--- E3P1TipusDeDadesCompostes/E1MostraUnArray.java
+++ E3P1TipusDeDadesCompostes/E1MostraUnArray.java
@@ -1,6 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+/** L'objectiu d'aquesta activitat és aprendre a recórrer un array per tractar els
+ * valors que conté.
+ *
+ * Feu un programa en Java anomenat MostraArray que mostri per pantalla el contingut
+ * d'un array, definit com a variable dins del programa de la següent manera:
+ *
+ *  -int array[] = {1, 12, 4, 5, 7, 9, 0, 9, 6, 7};
  */
 package E3P1TipusDeDadesCompostes;
 
@@ -9,5 +13,13 @@ package E3P1TipusDeDadesCompostes;
  * @author ruben
  */
 public class E1MostraUnArray {
-    
+
+    public static void main(String[] args) {
+
+        int array[] = {1, 12, 4, 5, 7, 9, 0, 9, 6, 7};
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
 }
