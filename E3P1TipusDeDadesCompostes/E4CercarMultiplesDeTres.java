5a29293918407e0f954df335f63a4d73e87355a6
Ruben <rubengonzalezglez@me.com>
2023-01-28 10:46:13 +0100

    Iniciado E4 Cercar multiples de tres

---
+++ E3P1TipusDeDadesCompostes/E4CercarMultiplesDeTres.java
@@ -0,0 +1,25 @@
+/** L'objectiu d'aquesta activitat és aprendre a recórrer un array per tractar els
+ * valors que conté.
+ *
+ * Feu un programa en Java anomenat CercaTres que mostri per pantalla el segon
+ * múltiple de 3 que trobi en un array. Cal definir l'array dins del método
+ * principal.
+ *
+ * Proveu el resultat usant les definicions següents d'array, una per a cada
+ * execució diferent:
+ *
+ *  -//Primer proveu si funciona usant aquest array.
+ *      int array[] = {1, 12, 4, 5, 7, 3, 2, 9, 6, 7};
+ *
+ *  -//Després proveu si funciona usant aquest array.
+ *      int array[] = {1, 13, 4, 5, 7, 3, 2, 8, 5, 7};
+ */
+package E3P1TipusDeDadesCompostes;
+
+/**
+ *
+ * @author ruben
+ */
+public class E4CercarMultiplesDeTres {
+
+}
