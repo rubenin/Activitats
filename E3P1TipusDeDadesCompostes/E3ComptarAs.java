6a04b5a8338c3f6cf9372486852f4e996a019388
Ruben <rubengonzalezglez@me.com>
2023-01-28 10:36:37 +0100

    Iniciado E3 Comptar as

---
+++ E3P1TipusDeDadesCompostes/E3ComptarAs.java
@@ -0,0 +1,16 @@
+/** L'objectiu d'aquesta activitat és aprendre a recórrer un array per tractar els
+ * valors que conté
+ *
+ * Genereu un programa en Java anomenat ComptaAsArray que compti quantes lletres a
+ * conté un determinat array de caràcters. L'array per tractar pot ser qualsevol
+ * i el podeu declarar com una constatn dins del codi font
+ */
+package E3P1TipusDeDadesCompostes;
+
+/**
+ *
+ * @author ruben
+ */
+public class E3ComptarAs {
+
+}
