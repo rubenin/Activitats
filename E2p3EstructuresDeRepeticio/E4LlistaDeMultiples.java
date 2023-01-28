a7bd4cba1385b6c0a0e6e05d2fea8666b7294dc0
Ruben <rubengonzalezglez@me.com>
2023-01-28 09:56:50 +0100

    Añadido ejercicio buscar divisor

--- E2p3EstructuresDeRepeticio/E4LlistaDeMultiples.java
+++ E2p3EstructuresDeRepeticio/E4LlistaDeMultiples.java
@@ -1,6 +1,6 @@
 /** L'objectiu d'aquesta activitat és aprendre a usar estructures de repetició per
  * generar valors a partir d'un recorregut.
- * 
+ *
  * Feu un programa anomenat LlistaMultiples que llegeixi des del teclat dos nombres
  * enters positius n1 i n2, amb n1 < n2, i escrigui, començant des de n1, tots els
  * nombres enters que són múltiples de n1 més petits o iguals que n2 , en ordre
@@ -15,22 +15,33 @@ import java.util.Scanner;
  * @author ruben
  */
 public class E4LlistaDeMultiples {
-    
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
 
 //Inicializamos la entrada de teclado
-    Scanner lector = new Scanner(System.in);
-    
+        Scanner lector = new Scanner(System.in);
+
+//Inicializamos una variable llamada valor que muestre el inicio del contador
+        int valor;
 //Solicitamos el valor de n1
-    System.out.print("Quin és el valor de n1?");
-    int n1 = lector.nextInt();
-    lector.nextLine();
-    
+        System.out.print("Quin és el valor de n1?");
+        int n1 = lector.nextInt();
+        lector.nextLine();
+
 //Solicitamos el valor de n2 (menor que n1)
-    System.out.print("Quin és el valor de n2 (més gran que n1)? ");
-    int n2 = lector.nextInt();
-    lector.nextLine();
-    
-    
+        System.out.print("Quin és el valor de n2 (més gran que n1)? ");
+        int n2 = lector.nextInt();
+        lector.nextLine();
+
+//Creamos el condicional para comprobar que n1 es menor que n2
+        if (n1 < n2) {
+            valor = n1;
+            for (int i = 2; valor <= n2; i++) {
+                System.out.print(valor);
+                valor = n1 * i;
+            }
+        } else {
+            System.out.println("n2 no és més gran que n1!");
+        }
     }
 }
--- E2p3EstructuresDeRepeticio/E5CercarDivisor.java
+++ E2p3EstructuresDeRepeticio/E5CercarDivisor.java
@@ -1,13 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+/** L'objectiu d'aquesta activitat és aprendre a usar estructures de repetició per
+ * cercar valors concrest dins d'un rang.
+ *
+ * Feu un programa anomenat CercarDivisor que llegeixi des del teclat un nombre
+ * enter positiu major que 1 i n'escrigui el divisor positiu més gran diferent
+ * d'ell mateix i d'1. En cas de no trobar-ne cap, cal anunciar aquest fet amb un
+ * text. Per fer-ho, useu una variable de control de tipus semàfor.
  */
 package E2p3EstructuresDeRepeticio;
+
+import java.util.Scanner;
 
 /**
  *
  * @author ruben
  */
 public class E5CercarDivisor {
-    
+
+    public static void main(String[] args) {
+        Scanner lector = new Scanner(System.in);
+
+//Solicitamos el número a analizar        
+        System.out.print("Quin nombre vols analitzar? ");
+        int valor = lector.nextInt();
+        lector.nextLine();
+
+//Creamos el booleano
+        boolean trobat = false;
+        int divisor = valor - 1;
+
+        while ((!trobat) && (divisor > 1)) {
+            if ((valor % divisor) == 0) {
+                trobat = true;
+            } else {
+                divisor--;
+            }
+        }
+        if (divisor == 1) {
+            System.out.println("No s'ha trobat cap divisor. ");
+        } else {
+            System.out.println("El divisor és " + divisor + ".");
+        }
+    }
 }
